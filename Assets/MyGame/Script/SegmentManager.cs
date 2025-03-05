96666deaac70ec497ac046741bc85ed9eaf88dad
hamzic08 <muhammed.hamzic08@htl-salzburg.ac.at>
2025-03-05 09:10:17 +0100

    rotation

--- Assets/MyGame/Script/SegmentManager.cs
+++ Assets/MyGame/Script/SegmentManager.cs
@@ -1,22 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class SegmentManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private float currentRotationY = 180f;  
+    private float targetRotationY = 0f;     
+    public float rotationSpeed = 50f;      
+    private bool isRotating = false;       
+
     void Start()
     {
-        
+        GameObject display = GameObject.FindWithTag("segment");
+        display.transform.rotation = Quaternion.Euler(0f, currentRotationY, 0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Alpha1)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKeyDown(KeyCode.W) && !isRotating)
         {
-            GameObject target = GameObject.FindWithTag("linksO"); // Suche das GameObject mit dem Tag
+            isRotating = true;
+        }
+        if (isRotating)
+        {
+            GameObject display = GameObject.FindWithTag("segment");
+
+            display.transform.rotation = Quaternion.RotateTowards(
+                display.transform.rotation,
+                Quaternion.Euler(0f, targetRotationY, 0f),
+                rotationSpeed * Time.deltaTime
+            );
+
+            if (Mathf.Approximately(display.transform.rotation.eulerAngles.y, targetRotationY))
+            {
+                isRotating = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Alpha1)) 
+        {
+            GameObject target = GameObject.FindWithTag("linksO"); 
             GameObject target1 = GameObject.FindWithTag("mitte");
             GameObject target2 = GameObject.FindWithTag("unten");
             GameObject target3 = GameObject.FindWithTag("oben");
@@ -35,18 +59,18 @@ public class SegmentManager : MonoBehaviour
             || Input.GetKeyUp(KeyCode.Alpha0)) 
         {
             ResetSegmentRotation("linksO", 0f, 0f, 0f);
-            ResetSegmentRotation("mitte", 90f, 0f, 0f); // Ausgangsrotation X = 90
-            ResetSegmentRotation("unten", 90f, 0f, 0f); // Ausgangsrotation X = 90
-            ResetSegmentRotation("oben", 90f, 0f, 0f); // Ausgangsrotation X = 90
+            ResetSegmentRotation("mitte", 90f, 0f, 0f);
+            ResetSegmentRotation("unten", 90f, 0f, 0f); 
+            ResetSegmentRotation("oben", 90f, 0f, 0f); 
             ResetSegmentRotation("linksU", 0f, 0f, 0f);
             ResetSegmentRotation("rechtsU", 0f,0f, 0f);
             ResetSegmentRotation("rechtsO", 0f, 0f, 0f);
 
         }
 
-        if (Input.GetKey(KeyCode.Alpha2)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKey(KeyCode.Alpha2)) 
         {
-            GameObject target = GameObject.FindWithTag("linksO"); // Suche das GameObject mit dem Tag
+            GameObject target = GameObject.FindWithTag("linksO"); 
             GameObject target2 = GameObject.FindWithTag("rechtsU");
 
             target.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
@@ -54,18 +78,18 @@ public class SegmentManager : MonoBehaviour
         }
 
 
-        if (Input.GetKey(KeyCode.Alpha3)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKey(KeyCode.Alpha3)) 
         {
-            GameObject target = GameObject.FindWithTag("linksO"); // Suche das GameObject mit dem Tag
+            GameObject target = GameObject.FindWithTag("linksO"); 
             GameObject target2 = GameObject.FindWithTag("linksU");
 
             target.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
             target2.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
         }
 
-        if (Input.GetKey(KeyCode.Alpha4)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKey(KeyCode.Alpha4)) 
         {
-            GameObject target = GameObject.FindWithTag("linksU"); // Suche das GameObject mit dem Tag
+            GameObject target = GameObject.FindWithTag("linksU");
             GameObject target2 = GameObject.FindWithTag("unten");
             GameObject target3 = GameObject.FindWithTag("oben");
 
@@ -74,9 +98,9 @@ public class SegmentManager : MonoBehaviour
             target3.transform.rotation = Quaternion.Euler(0f, 90f, 90f);
         }
 
-        if (Input.GetKey(KeyCode.Alpha5)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKey(KeyCode.Alpha5))
         {
-            GameObject target = GameObject.FindWithTag("linksU"); // Suche das GameObject mit dem Tag
+            GameObject target = GameObject.FindWithTag("linksU"); 
             GameObject target2 = GameObject.FindWithTag("rechtsO");
 
 
@@ -84,14 +108,14 @@ public class SegmentManager : MonoBehaviour
             target2.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
         }
 
-        if (Input.GetKey(KeyCode.Alpha6)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKey(KeyCode.Alpha6)) 
         {
             GameObject target = GameObject.FindWithTag("rechtsO");
             target.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
 
         }
 
-        if (Input.GetKey(KeyCode.Alpha7)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKey(KeyCode.Alpha7)) 
         {
             GameObject target = GameObject.FindWithTag("linksU");
             GameObject target1 = GameObject.FindWithTag("linksO");
@@ -104,24 +128,24 @@ public class SegmentManager : MonoBehaviour
             target3.transform.rotation = Quaternion.Euler(0f, 90f, 90f);
         }
 
-        if (Input.GetKey(KeyCode.Alpha8)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKey(KeyCode.Alpha8)) 
         {
             ResetSegmentRotation("linksO", 0f, 0f, 0f);
-            ResetSegmentRotation("mitte", 90f, 0f, 0f); // Ausgangsrotation X = 90
-            ResetSegmentRotation("unten", 90f, 0f, 0f); // Ausgangsrotation X = 90
-            ResetSegmentRotation("oben", 90f, 0f, 0f); // Ausgangsrotation X = 90
+            ResetSegmentRotation("mitte", 90f, 0f, 0f); 
+            ResetSegmentRotation("unten", 90f, 0f, 0f);
+            ResetSegmentRotation("oben", 90f, 0f, 0f);
             ResetSegmentRotation("linksU", 0f, 0f, 0f);
             ResetSegmentRotation("rechtsU", 0f, 0f, 0f);
             ResetSegmentRotation("rechtsO", 0f, 0f, 0f);
         }
 
-        if (Input.GetKey(KeyCode.Alpha9)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKey(KeyCode.Alpha9)) 
         {
             GameObject target = GameObject.FindWithTag("linksU");
             target.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
         }
 
-        if (Input.GetKey(KeyCode.Alpha0)) // Prüft, ob die "1"-Taste gedrückt wurde
+        if (Input.GetKey(KeyCode.Alpha0)) 
         {
             GameObject target = GameObject.FindWithTag("mitte");
             target.transform.rotation = Quaternion.Euler(0f, 90f, 90f);
@@ -132,10 +156,8 @@ public class SegmentManager : MonoBehaviour
             GameObject target = GameObject.FindWithTag(tag);
             if (target != null)
             {
-                target.transform.rotation = Quaternion.Euler(x, y, z); // Setzt auf die richtige Ausgangsrotation zurück
+                target.transform.rotation = Quaternion.Euler(x, y, z); 
             }
         }
     }
-
-
 }
